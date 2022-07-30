0a03ab3626378aa0c982a862f1239ab5abab6e4d
saranbabu29 <sharanraj982@gmail.com>
2022-07-30 19:49:53 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake and Ladder");
+            int position = 0;
+            Console.WriteLine("Player position is " +position);
+        }
+    }
+}
